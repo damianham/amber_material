44d444436d0119d1e63b7123f138337c3f8708b4
Damian Hamill <damianham@gmail.com>
2019-02-13 16:17:21 +0700

    add resource test

---
+++ app/src/lib/__tests__/resource.test.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import Resource from '../js/resource';
+import jquery from 'jquery';
+
+jest.mock('jquery');
+window.$ = jquery;
+window.csrf_token = 'skdjhasdjhaksjdhaksjhdaksjh';
+beforeEach(() => jest.resetModules());
+
+it('calls $.ajax with the correct params', () => {
+  const products = [{id:1, name: 'Widget'}];
+  const resp = {data: products};
+
+  jquery.ajax.mockImplementation(() => Promise.resolve(resp))
+
+  let res = new Resource('/products')
+
+  res.all().then(resp => expect(resp.data).toEqual(products));
+
+  // Now make sure that $.ajax was properly called
+  expect($.ajax).toBeCalledWith({
+    method: 'GET',
+    url: '/products',
+    dataType: 'json'
+  });
+});
+
+it('gets a record via $.ajax', () => {
+  const product = {id:1, name: 'Widget'};
+  const resp = {data: product};
+
+  jquery.ajax.mockImplementation(() => Promise.resolve(resp))
+
+  let res = new Resource('/products')
+
+  res.get(1).then(resp => expect(resp.data).toEqual(product));
+
+  // Now make sure that $.ajax was properly called
+  expect($.ajax).toBeCalledWith({
+    method: 'GET',
+    url: '/products/1',
+    dataType: 'json'
+  });
+});
+
+
+it('creates a record via $.ajax', () => {
+  const product = {name: 'Widget'};
+  const resp = {data: product};
+
+  jquery.ajax.mockImplementation(() => Promise.resolve(resp))
+
+  let res = new Resource('/products', { name: 'Widget'})
+
+  res.save().then(resp => expect(resp.data).toEqual(product));
+
+  // Now make sure that $.ajax was properly called
+  expect($.ajax).toBeCalledWith({
+    data: product,
+    method: 'POST',
+    url: '/products',
+    dataType: 'json',
+    headers: {
+      "X-CSRF-TOKEN": window.csrf_token
+    },
+  });
+});
+
+it('saves an existing record via $.ajax', () => {
+  const product = {id:1, name: 'Widget'};
+  const resp = {data: product};
+
+  jquery.ajax.mockImplementation(() => Promise.resolve(resp))
+
+  let res = new Resource('/products', {id:1, name: 'Widget'})
+
+  res.save().then(resp => expect(resp.data).toEqual(product));
+
+  // Now make sure that $.ajax was properly called
+  expect($.ajax).toBeCalledWith({
+    data: {...product, _method: 'patch'},
+    method: 'PUT',
+    url: '/products/1',
+    dataType: 'json',
+    headers: {
+      "X-CSRF-TOKEN": window.csrf_token
+    },
+  });
+});
+
+it('destroys an existing record via $.ajax', () => {
+  const product = {id:1, name: 'Widget'};
+  const resp = {data: product};
+
+  jquery.ajax.mockImplementation(() => Promise.resolve(resp))
+
+  let res = new Resource('/products', {id:1, name: 'Widget'})
+
+  res.destroy().then(resp => expect(resp.data).toEqual(product));
+
+  // Now make sure that $.ajax was properly called
+  expect($.ajax).toBeCalledWith({
+    method: 'DELETE',
+    url: '/products/1',
+    dataType: 'json',
+    headers: {
+      "X-CSRF-TOKEN": window.csrf_token
+    },
+  });
+});
